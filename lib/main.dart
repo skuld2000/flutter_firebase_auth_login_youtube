a4df1bbb9f74f8d20539aedd640aec28aa77a0c1
skuld2000 <skuld2000@hanmail.net>
2020-09-07 18:07:29 +0900

    Add Splash class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_auth_login_youtube/screens/main_page.dart';
 import 'data/join_or_login.dart';
 import 'screens/login.dart';
 import 'package:provider/provider.dart';
@@ -9,8 +12,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ChangeNotifierProvider<JoinOrLogin>.value(
-          value: JoinOrLogin(), child: AuthPage()),
+      home: Splash(),
     );
+  }
+}
+
+//로그인, 로그아웃 여부에 따라 다른 화면을 보여줌
+class Splash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Initialize FlutterFire
+    Firebase.initializeApp();
+
+    return StreamBuilder<User>(
+        //authStateChanges : 로그인, 로그아웃 변경사항을 Stream<User> 로 리턴
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.data == null) {
+            //로그인 정보 없으면
+            return ChangeNotifierProvider<JoinOrLogin>.value(
+                value: JoinOrLogin(),
+                //로그인, 가입 페이지
+                child: AuthPage());
+          } else {
+            //로그인 정보 있으면
+            return MainPage(email: snapshot.data.email);
+          }
+        });
   }
 }
